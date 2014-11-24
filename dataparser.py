55e3c6878cce56101d8e29872bda18da17bf1434
ermoso5 <zara.alaverdyan@gmail.com>
2014-11-24 11:50:26 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	dataparser.py

---
+++ dataparser.py
@@ -0,0 +1,272 @@
+<<<<<<< HEAD:data.py
+import os
+import re
+
+class Parser:
+    def __init__(self, outputdir):
+        self.outputdir = outputdir
+
+    def split(self, filename, chunksize=4024):
+        rest = ''
+        file = open(filename, encoding="utf-8")
+        count=0
+
+        while 1:
+            chunk = file.read(chunksize)
+
+            if not chunk and not rest:
+                break
+
+            parts = re.split("\[PubMed.*?\]", (rest+chunk))
+            length = len(parts)
+            if length >= 2:
+                r = range(0, length-1)
+            else:
+                r = range(0, length)
+
+            for i in r:
+                block = parts[i].strip().split('\n\n')
+                l = len(block)
+                skipIndex = block[0].find('.')
+                str = ' '.join(block[0:l-2])
+                try:
+                    year = re.search(r'\d{4}', str[skipIndex:]).group()
+                except:
+                    year = "unknown"
+                try:
+                    pmid = (re.search(r'PMID: [\d]+', block[l-1]).group())[6:]
+                except:
+                    pmid = "not_found_in_"+''.join(block[0][0:skipIndex])
+
+                year_dir =  os.path.join(self.outputdir, year)
+
+                if not os.path.exists(year_dir):
+                    os.makedirs(year_dir)
+
+                with open(os.path.join(year_dir, pmid+".txt"),"w+", encoding="utf-8") as newPub:
+                    newPub.write(block[l-2])
+                    newPub.close()
+                    count+=1
+                    print(count)
+            if length > 1:
+                rest = parts[length-1]
+            else:
+                rest = ""
+
+    def splitMedline(self, filename, chunksize=4024):
+        rest = ''
+        file = open(filename, encoding="utf-8")
+        yearField = ['DP  -', 'DP -', 'DP-']
+        abstractField = ['AB  -', 'AB -', 'AB-']
+        keywordField = ['MH  -', 'MH -', 'MH-' ]
+
+        while 1:
+            chunk = file.read(chunksize)
+            if not chunk and not rest:
+                break
+
+            docs = re.split("PMID-", (rest+chunk).strip())
+            length = len(docs)
+            if length > 1:
+                medLineLength = length-1
+            else:
+                medLineLength = length
+
+            field = re.compile(r'\n(\b\w{2,4}\b\s{0,2}-)')
+            for d in range(0, medLineLength):
+                doc = re.split(field, docs[d])
+                keywordsFound = False
+
+                #find pmid
+                #skip medlines with no pmid
+                if not doc[0]:
+                    continue
+                pmid = doc[0].strip()
+
+                index = self.findOneOf(doc, keywordField)
+                if index != -1:
+                    abstract = ""
+                    i = index
+                    while doc[i].startswith("MH"):
+                        abstract += doc[i+1]
+                        i+=2
+                    keywordsFound = True
+
+                if not keywordsFound:
+                    index = self.findOneOf(doc, abstractField)
+                    if index == -1:
+                        continue
+                    else:
+                        abstract = doc[index+1]
+
+                index = self.findOneOf(doc, yearField)
+                if index > -1:
+                    year = doc[index+1].strip()[0:4]
+                else:
+                    year = "unknown"
+                    print(docs[d])
+                    print("-------------------------")
+
+                year_dir = os.path.join(self.outputdir, year)
+
+                if not os.path.exists(year_dir):
+                    os.makedirs(year_dir)
+
+                with open(os.path.join(year_dir, pmid+".txt"),"w+", encoding="utf-8") as newPub:
+                    newPub.write(abstract)
+                    newPub.close()
+
+            if length > 1:
+                rest = docs[length-1]
+            else:
+                rest = ""
+
+    def findOneOf(self, list, listToFind):
+        for l in listToFind:
+            if l in list:
+                return list.index(l)
+        return -1
+
+#usage
+#Parser("D:/corpus/output").splitMedline("C:/Users/Zara/Downloads/medline.txt")
+=======
+__author__ = 'Zara'
+
+import os
+import re
+
+DEBUG = False
+
+class Parser:
+    def __init__(self, outputdir):
+        self.outputdir = outputdir
+        if not os.path.exists(outputdir):
+            os.makedirs(outputdir)
+
+    def split(self, filename, chunksize=4024):
+        rest = ''
+        file = open(filename, encoding="utf-8")
+        count=0
+
+        while 1:
+            chunk = file.read(chunksize)
+
+            if not chunk and not rest:
+                break
+
+            parts = re.split("\[PubMed.*?\]", (rest+chunk))
+            length = len(parts)
+            if length >= 2:
+                r = range(0, length-1)
+            else:
+                r = range(0, length)
+
+            for i in r:
+                block = parts[i].strip().split('\n\n')
+                l = len(block)
+                skipIndex = block[0].find('.')
+                str = ' '.join(block[0:l-2])
+                try:
+                    year = re.search(r'\d{4}', str[skipIndex:]).group()
+                except:
+                    year = "unknown"
+                try:
+                    pmid = (re.search(r'PMID: [\d]+', block[l-1]).group())[6:]
+                except:
+                    pmid = "not_found_in_"+''.join(block[0][0:skipIndex])
+
+                year_dir =  os.path.join(self.outputdir, year)
+
+                if not os.path.exists(year_dir):
+                    os.makedirs(year_dir)
+
+                with open(os.path.join(year_dir, pmid+".txt"),"w+", encoding="utf-8") as newPub:
+                    newPub.write(block[l-2])
+                    newPub.close()
+                    count+=1
+                    print(count)
+            if length > 1:
+                rest = parts[length-1]
+            else:
+                rest = ""
+
+    def splitMedline(self, filename, chunksize=4024):
+        rest = ''
+        file = open(filename, encoding="utf-8")
+        yearField = ['DP  -', 'DP -', 'DP-']
+        abstractField = ['AB  -', 'AB -', 'AB-']
+        keywordField = ['MH  -', 'MH -', 'MH-' ]
+
+        print("...splitting corpus into documents")
+         
+        while 1:
+            chunk = file.read(chunksize)
+            if not chunk and not rest:
+                break
+
+            docs = re.split("PMID-", (rest+chunk).strip())
+            length = len(docs)
+            if length > 1:
+                medLineLength = length-1
+            else:
+                medLineLength = length
+
+            field = re.compile(r'\n(\b\w{2,4}\b\s{0,2}-)')
+            for d in range(0, medLineLength):
+                doc = re.split(field, docs[d])
+                keywordsFound = False
+
+                #find pmid
+                #skip medlines with no pmid
+                if not doc[0]:
+                    continue
+                pmid = doc[0].strip()
+
+                index = self.findOneOf(doc, keywordField)
+                if index != -1:
+                    abstract = ""
+                    i = index
+                    while doc[i].startswith("MH"):
+                        abstract += doc[i+1]
+                        i+=2
+                    keywordsFound = True
+
+                if not keywordsFound:
+                    index = self.findOneOf(doc, abstractField)
+                    if index == -1:
+                        continue
+                    else:
+                        abstract = doc[index+1]
+
+                index = self.findOneOf(doc, yearField)
+                if index > -1:
+                    year = doc[index+1].strip()[0:4]
+                else:
+                    year = "unknown"
+                    if DEBUG:
+                        print(docs[d])
+                        print("-------------------------")
+
+                year_dir = os.path.join(self.outputdir, year)
+
+                if not os.path.exists(year_dir):
+                    os.makedirs(year_dir)
+
+                with open(os.path.join(year_dir, pmid+".txt"),"w+", encoding="utf-8") as newPub:
+                    newPub.write(abstract)
+                    newPub.close()
+
+            if length > 1:
+                rest = docs[length-1]
+            else:
+                rest = ""
+
+    def findOneOf(self, list, listToFind):
+        for l in listToFind:
+            if l in list:
+                return list.index(l)
+        return -1
+
+#usage
+#Parser("D:/corpus/output").splitMedline("C:/Users/Zara/Downloads/medline.txt")
+>>>>>>> origin/master:dataparser.py
---
+++ processor.py
@@ -0,0 +1,241 @@
+"""
+This class provides the preprocessing functions for the PubMed corpus:
+    processFolder(root, target, stemming, min_word_length, remove_numbers, remove_duplicates, ner)
+    processString(string, stemming, min_word_length, remove_numbers, remove_duplicates, ner)
+"""
+
+import os
+import time
+import requests
+import json
+
+from nltk import stem
+from adeptner import Adeptner
+from graph import Graph
+
+__author__ = "Marcello Bendetti"
+__status__ = "Prototype"
+
+
+DEBUG = False
+     
+EXCLUDE = ['!', '?', '.', ',', ':', ';', '_', '-', '+', '*', '/', '\\', '^', 
+"'", '"', '’' , '(', ')', '[', ']', '=', '°', '|', '{', '}', '\n', '\t',
+'%', '¡', '¨', '“', '”', '`', '<', '>', '$', '&', '@', '#', '°']
+
+STOP_WORDS = ['a', 'able', 'about', 'above', 'across', 'after', 'again', 'against', 'all', 'almost', 
+'also', 'am', 'among', 'an', 'and', 'any', 'are', 'as', 'at', 'be', 'because', 'been', 'before', 
+'being', 'below', 'between', 'both', 'but', 'by', 'can', 'cannot', 'could', 'dear', 'did', 'do', 
+'does', 'doing', 'don', 'down', 'during', 'each', 'either', 'else', 'ever', 'every', 'few', 'for', 
+'from', 'further', 'get', 'got', 'had', 'has', 'have', 'having', 'he', 'her', 'here', 'hers', 
+'herself', 'him', 'himself', 'his', 'how', 'however', 'i', 'if', 'in', 'into', 'is', 'it', 'its', 
+'itself', 'just', 'least', 'let', 'like', 'likely', 'may', 'me', 'might', 'more', 'most', 'must', 
+'my', 'myself', 'neither', 'no', 'nor', 'not', 'now', 'of', 'off', 'often', 'on', 'once', 'only', 
+'or', 'other', 'our', 'ours', 'ourselves', 'out', 'over', 'own', 'rather', 's', 'said', 'same', 
+'say', 'says', 'she', 'should', 'since', 'so', 'some', 'such', 't', 'than', 'that', 'the', 'their', 
+'theirs', 'them', 'themselves', 'then', 'there', 'these', 'they', 'this', 'those', 'through', 'thus', 
+'to', 'too', 'under', 'until', 'up', 'us', 'very', 'wants', 'was', 'we', 'were', 'what', 'when', 
+'where', 'which', 'while', 'who', 'whom', 'why', 'will', 'with', 'would', 'yet', 'you', 'your', 
+'yours', 'yourself', 'yourselves']
+
+PREFIXES = ['von', 'de', 'vant', 'van', 'der', 'vom', 'vander', 'zur',
+'ten', 'la', 'du', 'ter', 'dos', 'al', 'del', 'st', 'le', 'dos', 'da', 
+'do', 'mc', 'des', 'den', 'di', 'abu', 'vander', 'den', 'della', 'vande', 
+'dit', 'bin', 'ibn', 'el', 'los', 'dello', 'vanden', 'ap', 'las', 'delli', 
+'mac', 'mrs', 'mr', 'miss', 'jr', 'sr', 'II', 'III', 'IV', 'in']  
+     
+     
+class Processor(object):
+
+    def __init__(self):
+        self.exclude = EXCLUDE
+        self.stop_words = STOP_WORDS
+        self.prefixes = PREFIXES    #currently prefixes are not used
+        try:
+            self.ner = Adeptner()
+            self.ner_mode = 'offline'
+        except:
+            print("WARNING: you are using an online NER!")
+            self.ner_mode = 'online'
+            
+    
+    def folderToGraph(self, root="corpus", target_folder=None, graph=None, ner=False, stemming='medium', min_word_length=1):
+        """
+        Creates a target folder and related subdirectories according to the structure of the root folder. 
+        Then, processes all the textual files in the root structure and store the results in the target structure.
+        
+        :root               The root folder contains years subfolders that in turn contain the '.txt' documents.
+        :target_folder      The folderwhere to store the processed files.
+        :graph              The graph object.
+        :ner                Use named entity recognition.
+        :stemming           Choose whether or not applying stemming. It may be None, 'light', 'medium', 'heavy'. 
+        :min_word_length    Minimum number of characters for a word to be kept. 
+        :return             None
+        """
+        if not os.path.isdir(root):
+            raise Exception("'{0}' folder doesn't exist".format(root))
+        if graph:
+            if not isinstance(graph, Graph):
+                raise Exception("The graph object is non valid")
+            graph.connect()
+        if target_folder and not os.path.isdir(target_folder):       #create 'target' folder if it doesn't exist
+            os.mkdir(target_folder)            
+            
+        print("...preprocessing documents and building the graph")
+        count_nodes = 0 
+        count_edges = 0
+              
+        for dir in os.listdir(root):                            #visit year subdirectories of 'root' folder
+            dir_origin = os.path.join(root, dir)
+            if os.path.isdir(dir_origin) and dir.isdigit():     #don't visit directories whose name is not a year
+                layer = int(dir)                                #layer becames the year
+                
+                if target_folder:
+                    dir_dest = os.path.join(target_folder, dir)
+                    if not os.path.isdir(dir_dest):             #create year subfolders if they don't exist in 'target' 
+                        os.mkdir(dir_dest)
+       
+                for file in os.listdir(dir_origin):             #open textual files under year folder and process them
+                    if file.endswith(".txt"):               
+                        string = self.fileToString(os.path.join(dir_origin, file))              #put file into string
+                        entities = self.processString(string, ner, stemming, min_word_length)
+                        
+                        if entities: 
+                            if DEBUG:
+                                print(entities)
+                            if graph:                               
+                                cn, ce = graph.addToGraph(entities, int(layer))             #add to graph with year as layer
+                                count_nodes += cn
+                                count_edges += ce
+                            if target_folder:
+                                self.stringToFile(os.path.join(dir_dest, file), entities)   #save string to file
+                graph.commit()
+        graph.close()
+        print("added {0} nodes to {1} ".format(count_nodes, graph.graph_nodes))
+        print("added {0} edges to {1} ".format(count_edges, graph.graph_edges))   
+
+
+    def processString(self, string, ner=False, stemming=None, min_word_length=1):
+        """ 
+        Apply prerpocessing to a string.
+        :ner                Use named entity recognition (if True) or text preprocessing (if False) to extract entities.
+        :stemming           Choose whether or not applying stemming. It may be None, 'light', 'medium', 'heavy'. 
+        :min_word_length    Minimum number of characters for a word to be kept. 
+        :return             Cleaned up string
+        """
+        entities = []
+        result = []
+        
+        if ner:
+            entities = self.getEntities(string)                     #perform named entity recognition 
+        else:
+            string = self.removeStopwords(string)                   #remove the stopwords in STOP_WORDS
+            entities = string.split(" ")                            #this creates a bag of words        
+        
+        if entities:    
+            for ent in entities:
+                ent = self.cleanText(ent)                             #remove punctuation and numbers
+                if stemming:
+                    ent = self.stemText(ent, intensity=stemming)        #perform stemming
+                if min_word_length > 0:
+                    ent = self.removeShortWords(ent, min_word_length)   #remove short words
+                ent = ent.strip()                                       #remove trailing spaces
+                if ent:
+                    result.append(ent)
+            
+        result = list(set(result))                                  #remove duplicates
+        return result
+
+
+    def fileToString(self, file_path):
+        """Take the content of a file and put it in a string."""
+        text = ""
+        if file_path:
+            with open(file_path, "r") as f:
+                text = f.read() #omit size to read it all
+                f.close()
+        return text
+     
+     
+    def stringToFile(self, file_path, text):
+        """Take the content of a string and put it in a '.txt' file."""
+        if file_path and text:
+            with open(file_path, "w") as f:
+                f.write(text)
+                f.close()
+
+    
+    def getEntities(self, text):
+        """Apply Named Entity Recognition from Stanford's web service ADEPTA."""
+        if self.ner_mode == 'online':
+            ADEPT_url="http://dust.stanford.edu:8080/ADEPTRest/rest/annotate"
+            payload = {"adeptifyThis" : text}
+            r=requests.post(ADEPT_url, data=payload)
+            data = json.loads(r.text)
+            result = []
+            for row in data[0]["tokens"]:
+                if row["label"] == "MEDTERM":
+                    result.append(row["token"])
+                    print(row["token"])
+            return ' '.join(result)
+        else:
+            return self.ner.getTerms(text).get('MEDTERM')
+
+
+    def stemText(self, text, intensity):    
+        """Apply stemming to a string according to :intesity."""
+        #select nltk stemmer
+        if intensity is 'light':
+            s = stem.PorterStemmer()       
+        elif intensity is 'medium':
+            s = stem.snowball.EnglishStemmer()   
+        elif intensity is 'heavy':
+            s = stem.LancasterStemmer()
+        else:
+            raise Exception("'{0}' is not a correct intensity parameter. Must be light, medium or heavy.".format(intensity))
+        bow = text.split(" ")       #this creates a bag of words
+        result = []
+        for word in bow:
+            result.append(s.stem(word))
+        return ' '.join(result)
+
+
+    def cleanText(self, text):
+        """
+        1. remove the punctuation specified in EXCLUDE 
+        2. remove numbers
+        3. transform to lowercase
+        4. remove multiple spaces
+        """
+        new_text = ""
+        for c in text:
+            if c in self.exclude or c.isdigit():
+                new_text = new_text + " "
+            else:
+                new_text = new_text + c
+        new_text = new_text.lower()
+        return ' '.join(new_text.split())       #this removes double spaces
+        
+        
+    def removeShortWords(self, text, min_word_length):
+        """Remove words that are shorter than :min_word_length."""
+        bow = text.split(" ")       #this creates a bag of words
+        result = []
+        for word in bow:
+            if len(word)>=min_word_length:
+                result.append(word)
+        return ' '.join(result)
+
+
+    def removeStopwords(self, text):
+        """Remove the stopwords specified in STOP_WORDS."""
+        bow = text.split(" ")       #this creates a bag of words
+        result = []
+        for word in bow:
+            if word not in self.stop_words:
+                result.append(word)
+        return ' '.join(result)
+
+
+#if __name__=="__main__":
+#    debug()
