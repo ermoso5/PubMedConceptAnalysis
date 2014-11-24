8f8330ad67a59d1246492db8518efa15c61945bb
ermoso5 <zara.alaverdyan@gmail.com>
2014-11-24 12:19:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,46 @@
-from preprocessor import Preprocessor
-from data import Parser
+import time
+
+from dataparser import Parser
+from processor import Processor
 from graph import Graph
 
 DEBUG = True
 
-def main():
-   
-    #parsing
-    par = Parser("corpus")
-    par.splitMedline(filename="corpus/small_medline.txt")
-
-    #preprocessing
-    pp = Preprocessor()
-    pp.processFolder(root="corpus", 
-                     target="processed", 
-                     stemming='heavy', 
-                     min_word_length=8, 
-                     remove_duplicates=True, 
-                     remove_numbers=True,
-                     ner=True)
-    
-    #create graph here
+def main(parsing=True, processing=True, finalize=True):
+       
     g = Graph(graph_name="graph1")
-    g.makeFromFolder(root="processed")
-    
-    #debug 
+       
+    #STEP 1: parsing
+    if parsing:
+        start = time.process_time()
+        par = Parser(outputdir="corpus")     
+        par.splitMedline(filename="small_medline.txt")
+        print("Parsing done in {0}s".format(time.process_time()-start))
+        
+        
+    #STEP 2: preprocessing & building the graph
+    if processing:
+        start = time.process_time()
+        pp = Processor()
+        pp.folderToGraph(root="corpus", 
+                         graph=g,               #pass the graph object
+                         target_folder=None,    #don't store intermediate files #"processed"
+                         ner=True,
+                         stemming='heavy', 
+                         min_word_length=5)
+        print("Processsing done in {0}s".format(time.process_time()-start))
+        
+        
+    #STEP 3: finalization
+    if finalize:
+        start = time.process_time()
+        g.compressGraph()
+        print("Finalization done in {0}s".format(time.process_time()-start))
+        
+     
     if DEBUG:
         g.testGraph()
-    
- 
+
+     
 if __name__=="__main__":
     main()
-
