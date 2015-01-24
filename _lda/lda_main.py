9e2df70de31058a5a53d937304054f029d4129a1
nishara <nishara.pdn@gmail.com>
2015-01-24 22:04:56 +0200

    LDA changes update

--- _lda/lda_main.py
+++ _lda/lda_main.py
@@ -3,6 +3,7 @@ __author__ = 'Nishara'
 from _lda.lda import LDA
 
 train_lda_model = True
+root="../corpus"
 
 
 def main():
@@ -11,6 +12,7 @@ def main():
     if train_lda_model:
         lda.train_lda_model(num_topics=5, chunk_size=1000, passes=5, num_words_per_topic=30, root="../corpus")
 
+    lda.cluster_docs_by_year(root)
     lda.plot_concepts(2)
 
 if __name__=="__main__":
